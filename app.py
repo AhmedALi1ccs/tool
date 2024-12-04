363d7d944ebda831dd47ec528df6e724230fa45f
AhmedALi <ahmedali27428@gmail.com>
2024-12-04 22:36:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import io
 import json
 from placekey.api import PlacekeyAPI
 from io import StringIO
-
+st.cache_resource.clear()
 placekey_api_key = "iuFt9caOLW2XJFYLJLNt3Dc0BFQ300Pi"
 url = "https://storage.googleapis.com/rei_standradized/placekeys_standardized%20copy%207%20(2).csv"
 zrl = "https://storage.googleapis.com/rei_standradized/final_rei_11-13-2024_withPlacekeys.csv"
